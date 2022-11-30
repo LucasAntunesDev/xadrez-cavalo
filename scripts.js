92a78098a06d4041046cb66337d489f718c112b3
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2022-11-30 13:59:50 -0300

    movimentos possíveis (cavalo não está ficando no centro)

--- scripts.js
+++ scripts.js
@@ -1,95 +1,54 @@
-const create = (create) => document.createElement(create)
-$ = (id) => { return document.getElementById(id) }
-const posiciona = ({ x, y }) => console.log(x, y)
-const disable = (b) => b.disabled = true
 const verde = (v) => v.style.backgroundImage = 'linear-gradient(to top right, var(--green), var(--green2))'
+const disable = (b) => b.disabled = true
+
+const posiciona = ({ x, y }) => {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            // if (j == y || i == x) {
+            // if (j - x == x  || i - y == y) {
+            if (j - 2 == x && i - 1 == y ||
+                j - 3 == x && i == y ||
+                j == x && i - 1 == y ||
+                j + 1 == x && i == y ||
+                j + 1 == x && i +2 == y||
+                j == x && i +3 == y||
+                j -2== x && i +3 == y||
+                j -3== x && i +2 == y) {
+                // alert('arere')
+                verde(document.getElementById(`casa${i * 8 + j}`))
+                document.getElementById(`casa${i * 8 + j}`).disabled = false
+                // document.getElementById(`casa${i*8+j}`).innerHTML='Válido'
+            }
+        }
+    }
+    console.log(x, y)
+}
+
 const none = (n) => n.setAttribute('class', 'none')
-const btn = document.getElementsByClassName('casa')
-const tabuleiro = $('tabuleiro')
+const tabuleiro = document.getElementById('tabuleiro')
 
 for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-        const btn = create('button')
+        const btn = document.createElement('button')
         btn.setAttribute('type', 'button')
         btn.setAttribute('data-x', i)
         btn.setAttribute('data-y', j)
         let x = parseInt(btn.getAttribute('data-x'))
         let y = parseInt(btn.getAttribute('data-y'))
-        btn.setAttribute('id', 'casa' + x + '+' + y)
+        btn.setAttribute('id', `casa${i * 8 + j}`)
         let casa = parseInt(btn.getAttribute('id'))
         const color = (i + j) % 2 === 0 ? 'black' : 'white'
         btn.setAttribute('class', `casa ${color}`)
-        const img = create('img')
+        const img = document.createElement('img')
         img.src = 'cavalo.png'
         img.setAttribute('id', 'cavalo')
 
+
         btn.addEventListener('click', () => {
             posiciona(btn.dataset)
-            const pos = [[x, y]]
-            console.log(pos[0][0], pos[0][1])
+            btn.disabled = true
+            btn.appendChild(img)
 
-            //}
-            // if (btn.disabled == false) {
-            const xMaiorQueUmEMenorIgualACinco = x > 1 && x <= 5 ? true : false
-            const yMaiorIgualADoisEMenorIgualACinco = y <= 5 && x >= 2 ? true : false
-
-            // let casaNova1 = $(`casa${pos[0][0] + 1}+${pos[0][1] + 2}`).getAttribute('id')
-            let casaNova1 =null
-            $(`casa${pos[0][0] + 1}+${pos[0][1] + 2}`).getAttribute('id') != null ? casaNova1 = $(`casa${pos[0][0] + 1}+${pos[0][1] + 2}`).getAttribute('id') : ""
-            console.log(casaNova1)
-            let casaNova2 = $(`casa${pos[0][0] + 1}+${pos[0][1] - 2}`).getAttribute('id')
-            let casaNova3 = $(`casa${pos[0][0] - 1}+${pos[0][1] - 2}`).getAttribute('id')
-            let casaNova4 = $(`casa${pos[0][0] - 1}+${pos[0][1] + 2}`).getAttribute('id')
-            let casaNova5 = $(`casa${pos[0][0] + 2}+${pos[0][1] + 1}`).getAttribute('id')
-            let casaNova6 = $(`casa${pos[0][0] + 2}+${pos[0][1] - 1}`).getAttribute('id')
-            let casaNova7 = $(`casa${pos[0][0] - 2}+${pos[0][1] - 1}`).getAttribute('id')
-            let casaNova8 = $(`casa${pos[0][0] - 2}+${pos[0][1] + 1}`).getAttribute('id')
-
-            // casaNova8 = null
-            /*Se jogar de novo, a imagem do cavalo some
-                true ? none(img) : ''*/
-
-            if (xMaiorQueUmEMenorIgualACinco && yMaiorIgualADoisEMenorIgualACinco) {
-                // if (true) {
-                btn.disabled != true ? btn.appendChild(img) : ''
-                // casaNova1 = null
-                casaNova1 != null ? verde($(casaNova1)) : ''
-
-                casaNova2 != null ? verde($(casaNova2)) : ''
-
-                casaNova3 != null ? verde($(casaNova3)) : ''
-
-                casaNova4 != null ? verde($(casaNova4)) : ''
-
-                casaNova5 != null ? verde($(casaNova5)) : ''
-
-                casaNova6 != null ? verde($(casaNova6)) : ''
-
-                casaNova7 != null ? verde($(casaNova7)) : ''
-
-                casaNova8 != null ? verde($(casaNova8)): ''
-                /*Se jogar de novo, a imagem do cavalo some
-                    true ? none(img) : ''*/
-            } 
-            else if (x == 1) {
-                alert('meteu essa')
-                
-                casaNova1 != null ? verde($(casaNova1)) : ''
-
-                casaNova2 != null ? verde($(casaNova2)) : ''
-
-                casaNova3 != null ? verde($(casaNova3)) : ''
-
-                casaNova4 != null ? verde($(casaNova4)) : ''
-
-                casaNova5 != null ? verde($(casaNova5)) : ''
-
-                casaNova6 != null ? verde($(casaNova6)) : ''
-
-                casaNova7 != null ? verde($(casaNova7)) : ''
-
-                verde($(casaNova8))
-            }
         })
         tabuleiro.appendChild(btn)
     }
