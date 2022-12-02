f41e42f76917a46358102057e68db8b5b26db204
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2022-12-02 18:24:07 -0300

    permitido jogar apenas nas peças em verde

--- scripts.js
+++ scripts.js
@@ -5,17 +5,10 @@ const verde = (v) => v.classList.add('verde')
 const removerVerde = (r) => r.classList.contains('verde') ? r.classList.remove('verde') : ''
 const btnVerde = document.getElementsByClassName('verde')
 const diferenca = (a, b) => Math.abs(a - b)
-const teste = () => document.getElementById('casa16').style.border = 'solid 4px red'
 
-//x += 2, y ++
 const posiciona = ({ x, y }) => {
     for (let coluna = 0; coluna < 8; coluna++) {
         for (let linha = 0; linha < 8; linha++) {
-
-            //x+j = x && y+j=y+2;
-            // if (Math.abs(j) - 2 == Math.abs(x) && i - 1 == Math.abs(y) 
-            // if(y+x == y+2 || x+1==x+1) {
-
             if (/*Baixo Direita 1*/coluna - 2 == Math.abs(x) && (linha - 1) == Math.abs(y) ||
                 /*Baixo Direita 2*/coluna - 1 == Math.abs(x) && (linha - 2) == Math.abs(y) ||
                 /*Cima Direita 1*/coluna + 1 == Math.abs(x) && linha - 2 == Math.abs(y) ||
@@ -24,14 +17,6 @@ const posiciona = ({ x, y }) => {
                 /*Cima Esquerda 2*/ coluna + 1 == Math.abs(x) && (linha +  2) == Math.abs(y) ||
                 /*Baixo Esquerda 2*/coluna - 1 == Math.abs(x) && (linha + 2) == Math.abs(y)  ||
                 /*Baixo Esquerda 2*/coluna - 2 == Math.abs(x) && (linha + 1) == Math.abs(y)) {
-                // if (x-i == 2+j){
-                // if (linha == y || coluna2 == x){
-
-                //x=3 y=2
-                //1-1, 1-3
-                //(x-2)-
-
-                // if (coluna2==Math.abs(x-y) && linha == Math.abs(y)){
                 verde(document.getElementById(`casa${coluna * 8 + linha}`))
                 abled(document.getElementById(`casa${coluna * 8 + linha}`))
                 document.getElementById(`casa${coluna * 8 + linha}`).innerHTML = x + ' ; ' + y
@@ -67,11 +52,13 @@ for (let i = 0; i < 8; i++) {
 
             for (let i = 0; i < 8; i++) {
                 for (let j = 0; j < 8; j++) {
-                    // btn.classList.contains('') 
-                    removerVerde(btn)
+                    const botoes = document.getElementById(`casa${i * 8 + j}`) 
+                    botoes.classList.contains('abled') ? '' : botoes.classList.add('disabled')
+                    botoes.classList.contains('disabled') ? botoes.setAttribute('disabled', 'true') : ''
+                    botoes.classList.contains('verde') ? botoes.removeAttribute('disabled') : ''
+                    // removerVerde(botoes)
                 }
             }
-            teste()
         })
 
         tabuleiro.appendChild(btn)
