d6f914a003ff205cf9055bcf6c3fe8f5fc071831
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2022-12-09 16:28:31 -0300

    nova lógica

--- scripts.js
+++ scripts.js
@@ -1,104 +1,95 @@
-//Adiciona a classe abled
-const abled = (a) => a.classList.add('abled')
-//Adiciona a classe verde
-const verde = (v) => v.classList.add('verde')
-//Remove a cor verde de um botão
-const removerVerde = (r) => r.classList.contains('verde') ? r.classList.remove('verde') : ''
+const posiciona = ({ x, y }) => console.log(x, y)
 
-const posiciona = ({ x, y }) => {
-    //Prevê as possições possíveis para o cavalo se mover
-    for (let coluna = 0; coluna < 8; coluna++) {
-        for (let linha = 0; linha < 8; linha++) {
-            if (/*Baixo Direita 1*/coluna - 2 == Math.abs(x) && (linha - 1) == Math.abs(y) ||
-                /*Baixo Direita 2*/coluna - 1 == Math.abs(x) && (linha - 2) == Math.abs(y) ||
-                /*Cima Direita 1*/coluna + 1 == Math.abs(x) && linha - 2 == Math.abs(y) ||
-                /*Cima Direita 2*/coluna + 2 == Math.abs(x) && linha - 1 == Math.abs(y) ||
-                /*Cima Esquerda 1*/ coluna + 2 == Math.abs(x) && (linha + 1) == Math.abs(y) ||
-                /*Cima Esquerda 2*/ coluna + 1 == Math.abs(x) && (linha + 2) == Math.abs(y) ||
-                /*Baixo Esquerda 2*/coluna - 1 == Math.abs(x) && (linha + 2) == Math.abs(y) ||
-                /*Baixo Esquerda 2*/coluna - 2 == Math.abs(x) && (linha + 1) == Math.abs(y)) {
+let posicaoC = null;
+
+const tab = document.getElementById('tabuleiro');
+
+for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+        const d = document.createElement('button');
+        d.setAttribute('type', 'button');
+        d.setAttribute('id', `casa${i * 8 + j}`);
+        d.setAttribute('data-x', i);
+        d.setAttribute('data-y', j);
+        const color = (i + j) % 2 === 0 ? 'black' : 'white';
+        d.setAttribute('class', `casa ${color}`)
+        var jogadas = 0
+        var ultimaJogada = jogadas - 1
+        
+        d.addEventListener('click', () => {
+            posiciona(d.dataset)
+            if (posicaoC !== null) {
+                posicaoC.innerHTML = '';
+            }
+            d.innerHTML = '<img src="cavalo.png">';
+            posicaoC = d;
+
+            if (d.classList.contains('verde') && d.classList.contains(`jogada${jogadas - 1}`)) {
+                //Remove atributos se na próxima jogada
+                //a casa não for uma das possíveis para o
+                //cavalo se movimentar
+                d.classList.remove('jogada' + jogadas - 1)
+                d.classList.remove('verde')
+                d.classList.remove('abled')
+                d.classList.remove('jogada' + jogadas)
+
+            }
+
+            var x = parseInt(d.getAttribute('data-x'));
+            var y = parseInt(d.getAttribute('data-y'));
+            verificarPosicao(x, y);
+        })
+        
+        tab.appendChild(d)
+    }
+}
+
+const verificarPosicao = (xc, yc) => {
+    console.log(xc, yc);
+    for (let verifyX = 0; verifyX <= 8; verifyX++) {
+        for (let verifyY = 0; verifyY <= 8; verifyY++) {
+            if (/*Baixo Direita 1*/verifyY - 2 == Math.abs(xc) && (verifyX - 1) == Math.abs(yc) ||
+            /*Baixo Direita 2*/verifyY - 1 == Math.abs(xc) && (verifyX - 2) == Math.abs(yc) ||
+            /*Cima Direita 1*/verifyY + 1 == Math.abs(xc) && verifyX - 2 == Math.abs(yc) ||
+            /*Cima Direita 2*/verifyY + 2 == Math.abs(xc) && verifyX - 1 == Math.abs(yc) ||
+            /*Cima Esquerda 1*/ verifyY + 2 == Math.abs(xc) && (verifyX + 1) == Math.abs(yc) ||
+            /*Cima Esquerda 2*/ verifyY + 1 == Math.abs(xc) && (verifyX + 2) == Math.abs(yc) ||
+            /*Baixo Esquerda 2*/verifyY - 1 == Math.abs(xc) && (verifyX + 2) == Math.abs(yc) ||
+            /*Baixo Esquerda 2*/verifyY - 2 == Math.abs(xc) && (verifyX + 1) == Math.abs(yc)) {
+                let btn = document.getElementById(`casa${verifyX * 8 + verifyY}`);
+                // btn.style.backgroundImage = 'linear-gradient(to top right, var(--green), var(--green2))';
+
                 //Pinta a casa de verde
-                verde(document.getElementById(`casa${coluna * 8 + linha}`))
+                verde(document.getElementById(`casa${verifyY * 8 + verifyX}`))
+                // d.classList.contains('verde') ? '' : d.disabled = true
                 //Deixa a casa como disabled = false
-                abled(document.getElementById(`casa${coluna * 8 + linha}`))
-                document.getElementById(`casa${coluna * 8 + linha}`).disbabled = true ? document.getElementById(`casa${coluna * 8 + linha}`).disbabled = false : ''
+                abled(document.getElementById(`casa${verifyY * 8 + verifyX}`))
+                document.getElementById(`casa${verifyY * 8 + verifyX}`).disbabled = true ? document.getElementById(`casa${verifyY * 8 + verifyX}`).disbabled = false : ''
+
+                // verde(btn)
+                // removerVerde(btn)
             }
         }
     }
 }
+// const verificarPosicao = (xc, yc) => {
+//     console.log(xc, yc);
+//     for (let verifyX = 0; verifyX <= 8; verifyX++) {
+//         for (let verifyY = 0; verifyY <= 8; verifyY++) {
+//             if ((Math.abs(xc - verifyX) > 0 &&
+//                 Math.abs(yc - verifyY) > 0) &&
+//                 (Math.abs(xc - verifyX) <= 2 &&
+//                 Math.abs(yc - verifyY) <= 2)) {
+//                 let btn = document.getElementById(`casa${verifyX * 8 + verifyY}`);
+//                 // btn.style.backgroundImage = 'linear-gradient(to top right, var(--green), var(--green2))';
+//                 verde(btn)
+//                 // removerVerde(btn)
+//             }
+//         }
+//     }
+// }
 
-//Adiciona a classe none
-const none = (n) => n.classList.add('none')
 
-//Carrega o tabuleiro
-const tabuleiro = document.getElementById('tabuleiro')
-
-for (let i = 0; i < 8; i++) { //Linhas
-    for (let j = 0; j < 8; j++) { //Colunas
-        //Cria um botão e adiona atributos a ele
-        const btn = document.createElement('button')
-        btn.setAttribute('type', 'button')
-        btn.setAttribute('data-x', i)
-        btn.setAttribute('data-y', j)
-        btn.setAttribute('id', `casa${i * 8 + j}`)
-        const color = (i + j) % 2 === 0 ? 'black' : 'white'
-        btn.setAttribute('class', `casa ${color}`)
-        //Cria a imagem do cavalo e adiona atributos a ela
-        const img = document.createElement('img')
-        img.src = 'cavalo.png'
-        img.setAttribute('id', 'cavalo')
-
-        var jogadas = 0
-        var ultimaJogada = jogadas - 1
-        btn.addEventListener('click', () => {
-            jogadas < 1 ? btn.appendChild(img) : ''
-            img.classList.add('cavalo' + jogadas)
-            posiciona(btn.dataset)
-
-            //Verifica a cada posição 
-            let posicaoVerificada = btn.dataset
-            console.log('verified', posicaoVerificada)
-
-            for (let i = 0; i < 8; i++) {
-                for (let j = 0; j < 8; j++) {
-                    let botoes = document.getElementById(`casa${i * 8 + j}`)
-                    botoes.classList.contains('verde') ? botoes.classList.add(`jogada${jogadas}`) : ''
-
-                    let posicaoAtual = botoes.dataset
-                    console.log('atual', posicaoAtual)
-
-                    if (botoes.classList.contains('verde') && botoes.classList.contains(`jogada${jogadas - 1}`)) {
-                        //Remove atributos se na próxima jogada
-                        //a casa não for uma das possíveis para o
-                        //cavalo se movimentar
-                        botoes.classList.remove('jogada' + jogadas - 1)
-                        botoes.classList.remove('verde')
-                        botoes.classList.remove('abled')
-                        botoes.classList.remove('jogada' + jogadas)
-
-                    }
-
-                    //Verifica se a posição atual (verificada)
-                    //é a mesma casa que foi clicada
-                    if (posicaoAtual == posicaoVerificada) {
-                        console.log('sim')
-                        // img.style.display = 'none'
-                        botoes.appendChild(img)
-                    }
-
-                    //Verifica algumas condições, se verdadeiras adicionam/removem atributos
-                    //da casa
-                    botoes.classList.contains('jogada' + ultimaJogada) ? removerVerde(botoes) : ''
-                    botoes.classList.contains('abled') ? '' : botoes.classList.add('disabled')
-                    botoes.classList.contains('disabled') ? botoes.setAttribute('disabled', 'true') : ''
-                    botoes.classList.contains('verde') ? botoes.removeAttribute('disabled') : ''
-                    botoes.hasChildNodes ? abled(botoes) : ''
-                }
-            }
-            jogadas++
-        })
-        //Adiciona o botão como filho do tabueiro
-        tabuleiro.appendChild(btn)
-    }
-
-}
+const verde = (v) => v.classList.add('verde')
+const removerVerde = (r) => r.classList.contains('verde') ? r.classList.remove('verde') : ''
+const abled = (a) => a.classList.add('abled')
